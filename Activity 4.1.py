a829303663960d637ffbaef7a7346afa1a1d9459
theweirdowombat <kaitlynmariecookis@gmail.com>
2021-03-09 18:47:34 -0500

    fixed procedure fact

--- Activity 4.1.py
+++ Activity 4.1.py
@@ -13,6 +13,25 @@ def sum(y):
         y = y + i
     print(y)
 
+
 sum(100)
 
+
 print("\n")
+
+
+def fact(m):
+    t = 1
+    for i in range(m-1):
+        m = m * t
+        t = t + 1
+    print(m)
+
+fact(0)
+fact(1)
+fact(2)
+fact(3)
+fact(4)
+fact(5)
+
+
